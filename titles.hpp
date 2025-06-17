5f6a766f1deac8a5c2df39e648bd402c7202e1fd
MONGCHAW <provabben@gmail.com>
2025-06-18 03:32:44 +0600

    Updated based on new comments

--- titles.hpp
+++ titles.hpp
@@ -186,7 +186,7 @@ class RscTitles {
 			class RscWarlordsHUD_Rearm: RscStructuredText {
 				idc = 2104;
 				x = safeZoneW + safeZoneX - 0.45;
-				y = safeZoneH + safeZoneY - 0.25;
+				y = safeZoneH + safeZoneY - 0.27;
 				w = 0.2;
 				h = 0.05;
 				text = "";
@@ -195,7 +195,7 @@ class RscTitles {
 			class RscWarlordsHUD_Repair: RscStructuredText {
 				idc = 2105;
 				x = safeZoneW + safeZoneX - 0.25;
-				y = safeZoneH + safeZoneY - 0.25;
+				y = safeZoneH + safeZoneY - 0.27;
 				w = 0.2;
 				h = 0.05;
 				text = "";
